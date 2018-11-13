23da8196e8181a809e87aa3d4a6a4084e8734751
Ivan Hrynevytskyy <ivantuer@gmail.com>
2018-11-13 22:38:40 +0200

    Updates

--- precache-manifest.c917738325d8a55993c9a53d472c2f6c.js
+++ precache-manifest.c917738325d8a55993c9a53d472c2f6c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "4f3836f591203866a929",
-    "url": "/static/js/main.4f3836f5.chunk.js"
+    "revision": "df8d548ae81d982a9550",
+    "url": "/static/js/main.df8d548a.chunk.js"
   },
   {
     "revision": "2cd92b9bfbb839ffba6f",
     "url": "/static/js/1.2cd92b9b.chunk.js"
   },
   {
-    "revision": "4f3836f591203866a929",
-    "url": "/static/css/main.015f06f2.chunk.css"
+    "revision": "df8d548ae81d982a9550",
+    "url": "/static/css/main.c278d2f8.chunk.css"
   },
   {
-    "revision": "281bbc08bedbceb7cfa04c884fbe56b1",
+    "revision": "1bc99e5c8409623ba9bd2e265d78691e",
     "url": "/index.html"
   }
 ];
